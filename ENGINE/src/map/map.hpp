3e29cb8f100d6eca17ef0a79c56b6160c22f3fee
lucaszavattieriandrade-dev <lucaszavattieriandrade@gmail.com>
2026-04-27 09:31:14 +0200

    Update map.hpp
    montain added

--- ENGINE/src/map/map.hpp
+++ ENGINE/src/map/map.hpp
@@ -1,19 +1,53 @@
-#pragma once
-#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <time.h>
 
-typedef enum { Plain, Montain, Lake, Bush, ravine } TERRAIN;
-typedef enum { None1, Usine, Production, Resource } STRUCTURE;
-typedef enum { None2, soldier, archer, MONK } UNIT;
+#define X_Max 20
+#define Y_Max 20
 
-typedef struct {
+// montain related 
+#define Max_montain_quantity 4
+#define Max_montain_size 4
+#define thickness_max 4 // range off 1 to 4
+#define turne_chance_max 30
+#define stop_chance_max 35
+#define max_size 100
+//--
+
+
+typedef enum {Plain,Montain,Lake,Bush,ravine}TERRAIN;// 0plain ;1 montain ;2 lake ,3 tree ; 4 gold
+typedef enum {None_Struct,Usine,Production,Resource}STRUCTURE;
+typedef enum {None_Resource,tree,gold,Sapling}RESOURCE;
+
+typedef enum {None_Unit,archer,MONK}UNIT;
+
+typedef struct{
     TERRAIN type_terrain;
     STRUCTURE type_struct;
     UNIT type_unit;
-} CARRE;
+}CARRE;
 
-using MAP = std::vector<std::vector<CARRE>>;
+typedef struct MAP{
+    CARRE tab[X_Max][Y_Max];
+}MAP;
+// struct responsable for behavior of montains
+typedef struct Montain{
+    int x_init;
+    int y_init;
+    int size;
+    int DIR;//north northeast northwest east southeast southwest west south
+    int thickness;
+    int turne_chance;
+    int stop_chance;
+    
+}MONTAIN;
 
-MAP create_map(int width, int height);
-void generate_map(MAP& map);
-void affiche_map(const MAP& map);
-int terrain_size(const MAP& map, int x, int y);
+
+MAP *cree_map(void);
+MAP *genarete_map(MAP *map);
+void affiche_map(MAP *map_);
+int rac_TERRAIN_size_rec(MAP *map, int x, int y, TERRAIN type_, int **visited);
+int in_map(int x, int y);
+void set_terrain(MAP *map, int x, int y, TERRAIN terrain);
+MAP *crate_montain(MAP *map,MONTAIN montain[Max_montain_quantity]);
